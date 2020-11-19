a84a7af73ca9f641072be044ba62a5b1a3fb9974
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-11-19 16:06:55 -0500

    check deploy

--- client-v2/src/components/Dashboard.js
+++ client-v2/src/components/Dashboard.js
@@ -9,7 +9,7 @@ import Class from "./Class";
 import "../styles/DashboardCSS.css";
 
 export default function Dashboard() {
-  const inStructorClasses = useSelector((state) => state.instructorClasses);
+  const instructorClasses = useSelector((state) => state.instructorClasses);
   const [isEditing, setIsEditing] = useState(false);
   const [isAdding, setIsAdding] = useState(false);
 
@@ -17,7 +17,7 @@ export default function Dashboard() {
     <div className="dashboard">
       <UserCard />
       {JSON.parse(localStorage.getItem("user")).role === 2 &&
-        inStructorClasses.length > 0 &&
+        instructorClasses.length > 0 &&
         instructorClasses.map((cls) => {
           return !isEditing ? (
             <Class cls={cls} setIsEditing={setIsEditing} mutable={true} />
