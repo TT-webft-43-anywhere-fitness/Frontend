2fdd75aaecd634c30c6596aaa3fdf60d59d58591
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-11-19 08:47:51 -0500

    moved attendees actions to its own file

--- client-v2/src/store/actions/attendeesActions.js
+++ client-v2/src/store/actions/attendeesActions.js
@@ -1,6 +1,37 @@
-import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import { AxiosWithAuth } from "../../utils/axiosWithAuth";
 
-export const enroll = (classId, userId) => (dispatch) => {
-  return null
-  }
-}
+export const getAttendees = (classId) => (dispatch) => {
+  dispatch({ type: attendeesActionTypes.FETCH_ATTENDEES_START });
+  AxiosWithAuth()
+    .get(`${BASE_URL}/api/classes/${classId}/attendees`)
+    .then((res) => {
+      dispatch({
+        type: attendeesActionTypes.FETCH_ATTENDEES_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: attendeesActionTypes.FETCH_ATTENDEES_FAILURE,
+        payload: err.message,
+      });
+    });
+};
+
+export const enrollInClass = (classId, userId) => (dispatch) => {
+  dispatch({ type: attendeesActionTypes.ENROLL_IN_CLASS_START });
+  AxiosWithAuth()
+    .post(`${BASE_URL}/classes/${classId}/attendees`, userId)
+    .then((res) => {
+      dispatch({
+        type: attendeesActionTypes.ENROLL_IN_CLASS_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: attendeesActionTypes.ENROLL_IN_CLASS_FAILURE,
+        payload: err.message,
+      });
+    });
+};
--- client-v2/src/store/actions/classesActions.js
+++ client-v2/src/store/actions/classesActions.js
@@ -51,39 +51,3 @@ export const deleteClass = (classId) => (dispatch) => {
       dispatch({ type: actions.DELETE_CLASS_FAILURE, payload: err.message });
     });
 };
-
-export const getAttendees = (classId) => (dispatch) => {
-  dispatch({ type: attendeesActionTypes.FETCH_ATTENDEES_START });
-  AxiosWithAuth()
-    .get(`${BASE_URL}/api/classes/${classId}/attendees`)
-    .then((res) => {
-      dispatch({
-        type: attendeesActionTypes.FETCH_ATTENDEES_SUCCESS,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: attendeesActionTypes.FETCH_ATTENDEES_FAILURE,
-        payload: err.message,
-      });
-    });
-};
-
-export const enrollInClass = (classId, userId) => (dispatch) => {
-  dispatch({ type: attendeesActionTypes.ENROLL_IN_CLASS_START });
-  AxiosWithAuth()
-    .post(`${BASE_URL}/classes/${classId}/attendees`, userId)
-    .then((res) => {
-      dispatch({
-        type: attendeesActionTypes.ENROLL_IN_CLASS_SUCCESS,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: attendeesActionTypes.ENROLL_IN_CLASS_FAILURE,
-        payload: err.message,
-      });
-    });
-};
