08a7f68a98fb3f7ce9ee1cf24bbe86adc5b4c4da
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-11-19 14:40:21 -0500

    removed cors-anywhere

--- client-v2/src/utils/AxiosWithAuth.js
+++ client-v2/src/utils/AxiosWithAuth.js
@@ -3,7 +3,8 @@ import Axios from "axios";
 function AxiosWithAuth() {
   return Axios.create({
     baseURL:
-      "https://cors-anywhere.herokuapp.com/https://covid-bod.herokuapp.com/",
+      // "https://cors-anywhere.herokuapp.com/https://covid-bod.herokuapp.com/",
+      "https://covid-bod.herokuapp.com/",
     headers: {
       Authorization: localStorage.getItem("token"),
     },
